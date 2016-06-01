08167a548d80ec00068e5ca853882c7fc240b2e7
Nikola Stojiljkovic <nikola.stojiljkovic@essentialdots.com>
2016-06-01 12:31:05 +0200

    [TASK] Move composer.json to Packages folder to solve TYPO3 6.2 compatibility
    Change-Id: I60a30612b8773e609c3f6d6aa9ad62c26c003d82
    Related: #3956

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,12 @@ if (!defined('TYPO3_MODE')) {
 if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'EssentialDots\\EdMigrate\\Command\\EdMigrationCommandController';
 }
+
+$composerAutoloadPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY, 'Packages/vendor/autoload.php');
+$composerPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY, 'Packages/vendor');
+
+if (@is_dir($composerPath) && @is_file($composerAutoloadPath)) {
+	if (set_include_path($composerPath . PATH_SEPARATOR . get_include_path()) !== FALSE) {
+		require_once($composerAutoloadPath);
+	}
+}
