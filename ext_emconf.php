9b5126592ca1bb5ccbe6db1acc6e0902477d0ab2
Jenkins <jenkins@essentialdots.com>
2016-12-12 14:47:58 +0000

    [TASK] Set ed_migrate version to 0.0.13-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.0.12',
+	'version' => '0.0.13-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
