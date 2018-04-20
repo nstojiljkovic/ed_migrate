c9e3bc5eb504923c963dd5aea79e705b837d2b55
Jenkins <jenkins@essentialdots.com>
2018-04-20 19:02:25 +0000

    [RELEASE] Release of ed_migrate 0.0.19

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.0.19-dev',
+	'version' => '0.0.19',
 	'constraints' => 
 	array (
 	  'depends' => 
