c469ff7be7f5862741d627f9da19dec43a6059e4
Jenkins <jenkins@essentialdots.com>
2018-04-20 19:02:38 +0000

    [TASK] Set ed_migrate version to 0.0.20-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '0.0.19',
+	'version' => '0.0.20-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
